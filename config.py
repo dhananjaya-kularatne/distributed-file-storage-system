47eab1c81e58ced3865203bd0e2fc69872a65706
DhananjayaKularatne <djaya2100@gmail.com>
2026-03-17 21:09:51 +0530

    initial: add node configuration for distributed system

---
+++ config.py
@@ -0,0 +1,8 @@
+NODES = {
+    "node1": {"host": "127.0.0.1", "port": 5001},
+    "node2": {"host": "127.0.0.1", "port": 5002},
+    "node3": {"host": "127.0.0.1", "port": 5003},
+}
+
+BUFFER_SIZE = 4096
+STORAGE_DIR = "storage"
